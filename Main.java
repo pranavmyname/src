b054b08467525479aa3fd4a4d2624e37cab45b42
Pranav Prakash <pranavmyname@gmail.com>
2013-05-27 12:34:39 +0530

    Initial Commit

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
         Scanner accept=new Scanner(System.in);
         students s1=new students();
-        System.out.print("Enter the first name ");
+        System.out.println("Enter the first name ");
         s1.firstname=accept.next();
         System.out.println("Enter the last name ");
         s1.lastname=accept.next();
