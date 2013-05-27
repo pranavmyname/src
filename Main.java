9a311b29a1875f510c678b91bff1698f1ca64280
Pranav Prakash <pranavmyname@gmail.com>
2013-05-27 11:29:44 +0530

    git commit Main.java i

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class students{
+    String firstname;
+    String lastname;
+    }
+public class Main {
+    public static void main(String[] args){
+        Scanner accept=new Scanner(System.in);
+        students s1=new students();
+        System.out.print("Enter the first name ");
+        s1.firstname=accept.next();
+        System.out.println("Enter the last name ");
+        s1.lastname=accept.next();
+        System.out.println(s1.firstname+" "+s1.lastname);
+       // System.out.println(s1.lastname);
+    }
+}
