17fb9055773f397298e159f88dea2ab638fabd5e
Pranav Prakash <pranavmyname@gmail.com>
2013-05-27 12:48:39 +0530

    I commit

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         students s1=new students();
         System.out.println("Enter the first name ");
         s1.firstname=accept.next();
-        System.out.println("Enter the last name ");
+        System.out.println("Enter the lastru name ");
         s1.lastname=accept.next();
         System.out.println(s1.firstname+" "+s1.lastname);
        // System.out.println(s1.lastname);
